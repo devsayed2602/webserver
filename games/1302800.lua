bc0bb06fa510f7b6fd60be9b9e1486ccd8869329
sayedalimollah2602-prog <sayedalimollah2602@gmail.com>
2026-01-27 00:08:52 +0530

    games lua update

---
+++ games/1302800.lua
@@ -0,0 +1,8 @@
+-- ============================================
+--  Credits: K3rnelPan1c (KernelOS / !K3rnalyze)
+--  Discord: https://discord.gg/k3rnalyze
+--  Website: https://kernelos.org/games
+-- ============================================
+
+addappid(1302800)
+addappid(1302801,0,"49faf9189f206feeac4e224e8ee773bb3bd723ba57909739198da9530009f61b")
